58fb1f51d10881e5218daa9630b28ca6ebadec8b
rossdrew <thek1ng13@hotmail.com>
2022-07-27 14:11:36 +0100

    Initial Commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by Ross Drew on 27/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
